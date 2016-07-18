0b17b7f0af19930e2f9495309663e4ef4500c553
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-18 09:32:26 +0200

    Testing media queries

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>QuoteMaster</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <h1>QuoteMaster</h1>
