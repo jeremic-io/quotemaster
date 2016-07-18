a9d6be67e4edb46349760eb72a88bacd1908770a
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-18 10:37:24 +0200

    Render blocking css and javascript

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
   <meta name="description" content="Best quotes from famous people...">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>QuoteMaster</title>
+  <style>
+    h1 {
+      color:green;
+    }
+  </style>
 </head>
 <body>
 
