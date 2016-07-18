267362a2634ad90aa13b9e7b8f9bba494f82ffe8
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-07-18 12:07:29 +0200

    Render blocking css and javascript

--- index.php
+++ index.php
@@ -5,13 +5,41 @@
   <meta name="description" content="Best quotes from famous people...">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>QuoteMaster</title>
+
+  <style>
+    .intro {
+      background-image:url('images/landing.jpg');
+      background-position: top fixed;
+      background-attachment: fixed;
+      background-size: cover;
+      height:600px;
+    }
+
+  </style>
 </head>
 <body>
 
   <header class="main-header">
-    
+    <h1>QuoteMaster</h1>
   </header>
 
+  <section class="intro">
+    
+  </section>
+
+  <aside class="main-aside">
+
+  </aside>
+
+  <main class="main-content">
+
+  </main>
+
+  <footer class="main-footer">
+
+  </footer>
+
+  <!-- Javascript -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   <!-- defer loading of javascript files -->
   <script>
